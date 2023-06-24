f841cc69299ca0832fc04e0da5da44987715c842
takumi_machino <107854172+MachinoTakumi52@users.noreply.github.com>
2023-06-24 17:19:59 +0900

    hlmetとCORSを設定

--- mattchi-back-end/src/index.ts
+++ mattchi-back-end/src/index.ts
@@ -1,6 +1,8 @@
 import express, { Application } from "express";
 import { Server } from "http";
 import router from "./controller/router";
+import helmet from "helmet";
+import cors from "cors";
 
 const app: Application = express();
 const PORT = 3000;
@@ -15,6 +17,33 @@ app.use(express.urlencoded({ extended: true }));
 
 //ルーター設定
 app.use(router);
+
+//helmet(レスポンスヘッダ)設定
+app.use(
+  helmet({
+    //CSP(XSS攻撃の軽減やデータインジェクション攻撃の軽減をするために追加できるセキュリティレイヤ)設定
+    //ヘッダの値には、各リソース（JavaScript、CSS、画像など）を、どこから（自分のドメイン、特定の外部ドメイン、JSならscriptタグなど）
+    // 読み込むことを許可するのかが記述されている
+    contentSecurityPolicy: {
+      directives: {
+        //任意のドメインからの許可
+        // 「self」は同一オリジンかどうか
+        defaultSrc: ["'self'"],
+        //任意のドメインからのスクリプトの実行を許可
+        scriptSrc: ["'self'"],
+        //任意のドメインからの画像の読み込みを許可
+        imgSrc: ["'self'"],
+      },
+    },
+  })
+);
+
+//CORS設定
+app.use(
+  cors({
+    origin: ["http://example.com"], //許可するオリジン
+  })
+);
 
 try {
   server = app.listen(PORT, () => {
