d735849ce6538eb594a4167dedfaff3787122ee6
takaka-t <>
2023-08-26 15:13:28 +0900

    開発時のCORSエラー対策の設定について追記

--- README.md
+++ README.md
@@ -17,3 +17,8 @@ front-end
 ブレークポイントはブラウザ(Chrome)のデベロッパツールを使用
 
 ※どちらも保存で自動リロード有効
+
+# 開発時の CORS エラー対策の設定
+
+vite.config.ts に proxy 設定を行う
+https://ja.vitejs.dev/config/server-options.html#server-proxy
--- front-end/vite.config.ts
+++ front-end/vite.config.ts
@@ -1,14 +1,19 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    // 開発時のCORSエラー対策の設定
+    // https://ja.vitejs.dev/config/server-options.html#server-proxy
+    proxy: {},
+  },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
